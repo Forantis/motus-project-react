8a42570bd6c0d8f4b01cdc3c10bdf6103cc03fec
Forantis <antoine.forest.08@hotmail.fr>
2024-10-03 15:45:59 +0200

    Refactor Grid component to include counter for user input and handle backspace key

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { useState } from 'react'
+import { useEffect } from 'react'
 import Grid from './components/Grid'
 import Keyboard from './components/Keyboard'
 import RestartButton from './components/RestartButton'
@@ -12,11 +13,26 @@ function App() {
   // counter to count how many letter has been submit by the user to act as a submit
   const [counter, setCounter] = useState(0)
 
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Backspace') {
+      setResponse(prevResponse => prevResponse.slice(0, -1));
+      setCounter(prevCounter => Math.max(prevCounter - 1, 0));
+    }
+  };
+
+  // Add event listener for keydown
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <>
       <h1>Wordle From Wish</h1>
       <table>
-        <Grid searchedWord={searchedWord} response={response}/>
+        <Grid searchedWord={searchedWord} response={response} counter={counter}/>
       </table>
       <Keyboard response = {response} setResponse = {setResponse} counter={counter} setCounter={setCounter}/>
       <RestartButton setWord={setWord} />
