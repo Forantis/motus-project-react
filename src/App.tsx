99434cd464fbd1e65ace8b22bbeff55bb289592e
Forantis <antoine.forest.08@hotmail.fr>
2024-10-03 16:46:10 +0200

    Refactor App component to fix rowDone increment logic

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ function App() {
   const searchedWord = word.split('');
   const [response, setResponse] = useState([])
   // counter to count how many letter has been submit by the user to act as a submit
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(1)
   const [rowDone, setRowDone] = useState(0)
 
   // to follow the progress of the user and to know when to validate row
   useEffect(() => {
-    if(counter === (searchedWord.length-1) * rowDone+1) {
+    if(counter === searchedWord.length * rowDone+1) {
       setRowDone(rowDone + 1);
       console.log("rowDone", rowDone);
     }
